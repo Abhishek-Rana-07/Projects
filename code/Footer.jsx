e569640ba604fb51051df45178ba0029a85acd30
Abhishek-Rana-07 <91174377+Abhishek-Rana-07@users.noreply.github.com>
2022-07-24 10:13:30 +0530

    Add files via upload

---
+++ code/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+     <>
+        <footer className="bg-light text-center">
+        <p>@All Rights Reserved | Terms and Conditions applied</p>
+        </footer>
+     </>
+  )
+}
+
+export default Footer
